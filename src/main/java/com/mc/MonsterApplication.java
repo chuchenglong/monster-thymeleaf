559a0bb9ef33a07b516a04fb6351317723b9ef96
chuchenglong <martinchor@foxmail.com>
2018-04-20 19:42:50 +0800

    添加tomcat

--- src/main/java/com/mc/MonsterApplication.java
+++ src/main/java/com/mc/MonsterApplication.java
@@ -2,11 +2,18 @@ package com.mc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MonsterApplication {
+public class MonsterApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonsterApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MonsterApplication.class);
 	}
 }
